0530308c9b0324b45a8361a2232c7b48b5ab1b08
shuming <shuming@sslab.cs.nthu.edu.tw>
2013-07-19 21:09:57 +0800

    add icon, short url supported

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-document.getElementById('play-btn').onclick = function() {
+function $(s) { return document.getElementById(s); }
+
+$('play-btn').onclick = function() {
   var base_url = "http://query.yahooapis.com/v1/public/yql?q=";
-  var target_url = document.getElementById('url').value;
+  var target_url = $('url').value;
   var yql = "select * from html where url='" + target_url + "' and xpath='//a'";
   var url = base_url + encodeURIComponent(yql) + '&format=json&callback=parse';
 
@@ -8,27 +10,44 @@ document.getElementById('play-btn').onclick = function() {
   script.type = 'text/javascript';
   script.async = true;
   script.src = url;
-  document.getElementById('yql').appendChild(script);
+  $('yql').appendChild(script);
+  if($('icon').classList) {
+    $('icon').classList.remove('icon-play');
+    $('icon').classList.add('icon-spinner', 'icon-spin');
+  }
 };
 
 function parse(obj) {
   idSet = [];
   var idMap = {};
+  rr = obj;
   for(var i = 0; i < obj.query.count; i++) {
     var url = obj.query.results.a[i].href;
-    if(r = url.match(/https?:\/\/www.youtube.com\/watch\?v=(\w+)&?/)) {
-      var id = r[1];
-      if(id && !idMap.hasOwnProperty(id)) {
-        idSet.push(id);
-        idMap[id] = null;
+    if(url) {
+      var r = url.match(/https?:\/\/(www.youtube.com\/watch\?v=|youtu.be\/)(\w+)&?/);
+      if(r && r[2]) {
+        var id = r[2];
+        if(id && !idMap.hasOwnProperty(id)) {
+          idSet.push(id);
+          idMap[id] = null;
+        }
       }
     }
   }
 
-  player.loadPlaylist({
-    playlist: idSet,
-    suggestedQuality: 'hd720'
-  });
+  if(idSet.length != 0) {
+    player.loadPlaylist({
+      playlist: idSet,
+      suggestedQuality: 'hd720'
+    });
+  } else {
+    alert('There is no any youtube link');
+  }
+
+  if($('icon').classList) {
+    $('icon').classList.remove('icon-spinner', 'icon-spin');
+    $('icon').classList.add('icon-play');
+  }
 }
 
 (function() {
@@ -36,4 +55,6 @@ function parse(obj) {
   var atts = { id: "player" };
   swfobject.embedSWF("http://www.youtube.com/v/PLAYLIST_ID?enablejsapi=1&playerapiid=ytplayer&version=3",
                      "ytapiplayer", "800", "450", "8", null, null, params, atts);
+
+  $('url').focus();
 })();
